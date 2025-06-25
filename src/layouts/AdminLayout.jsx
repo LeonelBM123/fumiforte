d3fa8e3f7f5246ff9a2fa6d8967d748823ddc6d5
Leonel Barrios <leonel.barriosmay@gmail.com>
2025-06-25 01:30:58 -0400

    prueba 3

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -73,7 +73,7 @@ function AdminLayout() {
 
     setChatMessages((prev) => [
       ...prev,
-      { sender: "bot", text: data.response || "Sin respuesta" },
+      { sender: "bot", text: data.messages || "Sin respuesta" },
     ]);
   } catch (error) {
     console.error("Error al enviar mensaje:", error);
